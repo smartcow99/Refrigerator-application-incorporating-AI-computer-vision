9b62d314ef760a874eeeaafb078dd0112c8edd27
mangu <66214527+manguuu@users.noreply.github.com>
2021-08-04 15:01:20 +0900

    appbar

---
+++ lib/settingpage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  const SettingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // key: scaffoldKey,
+        theme: ThemeData(primaryColor: Colors.white),
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text("Setting"),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  print("button pressed");
+                  // final snackBar = SnackBar(content: const Text("Save Done!"));
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: const Text("Save Done!")));
+                  //Todo save all 구현
+                },
+                icon: Icon(Icons.save_alt),
+              )
+            ],
+          ),
+        ));
+  }
+}
