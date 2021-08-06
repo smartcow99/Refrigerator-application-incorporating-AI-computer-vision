5da184cb5e2af97b393b161493113a8a0a6c1ed9
yimo22 <yimo22@naver.com>
2021-08-07 01:29:45 +0900

    Final_Structure/ MainPage : my_main.dart Youtube Search Page : my_search.dart Info,setting Page : my_info.dart 로 부탁드리겠습니다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,84 @@
 import 'package:flutter/material.dart';
 
+import 'my_info.dart';
+import 'my_main.dart';
+import 'my_search.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Refrigeator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomepage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomepageState createState() => _MyHomepageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomepageState extends State<MyHomepage> {
+  var _pageIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // _children 은 각각의 Page 를 담는 List<Widget>
+  // ex. YoutubeSearchPage : class ytfPage 일경우
+  // ytfPage() 를 _children 에 추가한다.
+  // Main 페이지 : my_main.dart
+  // 유투브 검색 페이지 : my_search.dart
+  // 개인정보,설정 페이지 : my_info.dart
+  final List<Widget> _children = [const MyMain(),const MySearch(),const MyInfo()];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('[상단 제목]'),
+        centerTitle: true, // 중앙 정렬 여부
+        elevation: 6, // 상단바 그림자 강도
+        actions: <Widget>[
+          // MainPage 상단 버튼을 추가하려면 여기에 추가하세요.
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.add_circle_outline)),
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.add_circle_outline)),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: _children[_pageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (value) {
+          setState(() {
+            _pageIndex = value;
+          });
+        },
+        currentIndex: _pageIndex,
+        // 현재 페이지
+        selectedItemColor: Colors.amber,
+        // 선택된 NavigationBar의 색상
+        unselectedItemColor: Colors.black12,
+        // 선택안된 NavigationBar 의 색상
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Main page'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Youtube Search Page',
+            //   activeIcon: FavoriteListPage([])
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Myinfo Page'),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/my_search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySearch extends StatefulWidget {
+  const MySearch({Key? key}) : super(key: key);
+
+  @override
+  _MySearchState createState() => _MySearchState();
+}
+
+class _MySearchState extends State<MySearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
