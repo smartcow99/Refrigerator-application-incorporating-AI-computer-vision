f886cef400abf4452165c7b7bfd9cdb5c68627be
smartcow99 <78792358+smartcow99@users.noreply.github.com>
2021-08-11 22:39:10 +0900

    Add files via upload

---
+++ my_info.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyInfo extends StatefulWidget {
+  const MyInfo({Key? key}) : super(key: key);
+
+  @override
+  _MyInfoState createState() => _MyInfoState();
+}
+
+class _MyInfoState extends State<MyInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('This is Myinfo'),
+    );
+  }
+}
