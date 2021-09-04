b92ddbc49dff3154fd684c2dcd3c56f6f10e455b
mangu <66214527+manguuu@users.noreply.github.com>
2021-09-04 16:54:08 +0900

    json parsing

--- lib/my_main.dart
+++ lib/my_main.dart
@@ -11,6 +11,7 @@ import 'package:tflite/tflite.dart';
 import 'package:speed_dial_fab/speed_dial_fab.dart';
 
 import 'package:refrigerator/savePhotoData.dart';
+import 'dart:convert';
 
 Future<bool> checkPermission() async {
   Map<Permission, PermissionStatus> statuses = await [
@@ -37,7 +38,32 @@ class ListData {
       required this.expirationDate,
       required this.itemName});
 
+  factory ListData.fromJson(Map<String, dynamic> parsedJson) {
+    return ListData(
+        purchaseDate: parsedJson['purchaseDate'],
+        expirationDate: parsedJson['expirationDate'],
+        itemName: parsedJson['itemName']);
+  }
+  toJson() => {
+        'purchaseDate': this.purchaseDate,
+        'expirationDate': this.expirationDate,
+        'itemName': this.itemName
+      };
+
   String toString() => purchaseDate + "/" + expirationDate + "/" + itemName;
+}
+
+String listDatasToJson(List<ListData> listDatas) =>
+    jsonEncode(listDatas.map((i) => i.toJson()).toList()).toString();
+
+List<ListData> listDatasFromJson(String json) {
+  List<dynamic> parsedJson = jsonDecode(json);
+  print("parsedJson = $parsedJson");
+  List<ListData> listdatas = [];
+  for (int i = 0; i < parsedJson.length; i++) {
+    listdatas.add(ListData.fromJson(parsedJson[i]));
+  }
+  return listdatas;
 }
 
 class MyMain extends StatefulWidget {
